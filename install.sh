4f70ee7bdddf7eb30e055b35ebe17b898fd4d59c
madahin <nico99999@hotmail.fr>
2021-08-28 17:26:10 +0200

    Add kitty configuration + refactor installer

--- install.sh
+++ install.sh
@@ -12,8 +12,28 @@ function prog_exist()
     fi
 }
 
+function check_existing_conf()
+{
+    if [[ -f "${1}" ]]
+    then
+        echo "There is already a ${2} configuration on this system"
+        echo "Continuing while erase the existing configuration"
+
+        read -p "Do you whant to continue ? " -n 1 -r
+        echo    # (optional) move to a new line
+        if [[ $REPLY =~ ^[Yy]$ ]]
+        then
+            rm -f "${1}"
+            return 0
+        else
+            return 1
+        fi
+    fi
+}
+
 function install_nvim()
 {
+    conf_path="${XDG_DATA_HOME:-$HOME/.config/nvim}/init.vim"
     version=$(nvim -v | head -n1 | cut -d ' ' -f 2 | cut -c2-)
     echo "nvim version: ${version}"
 
@@ -23,19 +43,9 @@ function install_nvim()
         return 1
     fi
 
-    if [[ -f "${XDG_DATA_HOME:-$HOME/.config/nvim}/init.vim" ]]
+    if ! check_existing_conf ${conf_path} "neovim"
     then
-        echo "There is already a nvim configuration on this system"
-        echo "Continuing while erase the existing configuration"
-
-        read -p "Do you whant to continue ? " -n 1 -r
-        echo    # (optional) move to a new line
-        if [[ $REPLY =~ ^[Yy]$ ]]
-        then
-            rm -f "${XDG_DATA_HOME:-$HOME/.config/nvim}/init.vim"
-        else
-            return 1
-        fi
+        return 1
     fi
 
     echo checking for vim-plug
@@ -47,14 +57,30 @@ function install_nvim()
             https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
     fi
 
-    ln -s "${PWD}/init.vim" "${XDG_DATA_HOME:-$HOME/.config/nvim}/init.vim"
+    ln -s "${PWD}/init.vim" "${conf_path}"
 
     echo "Installation complete"
 
     return 0
 }
 
-for prog in nvim
+function install_kitty()
+{
+    conf_path="${XDG_DATA_HOME:-$HOME/.config/kitty}/kitty.conf"
+    
+    if ! check_existing_conf ${conf_path} "kitty"
+    then
+        return 1
+    fi
+
+    ln -s "${PWD}/kitty.conf" "${XDG_DATA_HOME:-$HOME/.config/kitty}/kitty.conf"
+    
+    echo "Installation complete"
+
+    return 0
+}
+
+for prog in nvim kitty
 do
     if prog_exist ${prog}
     then
