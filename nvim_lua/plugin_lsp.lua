73bd1d5d3f982533f482a1af7aca2308b2202e51
madahin <contact@madahindev.com>
2022-11-03 15:05:14 +0100

    Remove unused option for deprecated "tools.hover_with_actions"

--- nvim_lua/plugin_lsp.lua
+++ nvim_lua/plugin_lsp.lua
@@ -20,7 +20,6 @@ vim.cmd('set completeopt=menuone,noinsert,noselect')
 local opts = {
     tools = { -- rust-tools options
         autoSetHints = true,
-        hover_with_actions = true,
         inlay_hints = {
             show_parameter_hints = true,
             parameter_hints_prefix = "",
